dff1d889be672e773e98ee5e8ac9f7b7a9d82706
Raheenul Hasan <rhasan1@LHTU05CG8437CP4.ms.ds.uhc.com>
2021-04-11 21:17:14 +0530

    Adding sysout in Batch pkg

--- src/main/java/com/techprimers/springbatchexample1/batch/Processor.java
+++ src/main/java/com/techprimers/springbatchexample1/batch/Processor.java
@@ -1,27 +1,35 @@
-package com.techprimers.springbatchexample1.batch;
 
-import com.techprimers.springbatchexample1.model.User;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.stereotype.Component;
+package com.techprimers.springbatchexample1.batch;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import com.techprimers.springbatchexample1.model.User;
+
 @Component
-public class Processor implements ItemProcessor<User, User> {
+public class Processor implements ItemProcessor<User, User>
+{
 
-    private static final Map<String, String> DEPT_NAMES =
-            new HashMap<>();
+    private static final Map<String, String> DEPT_NAMES = new HashMap<String, String>();
 
-    public Processor() {
+    public Processor()
+    {
+        System.out.println("Entering Processor() of Processor.");
+
         DEPT_NAMES.put("001", "Technology");
         DEPT_NAMES.put("002", "Operations");
         DEPT_NAMES.put("003", "Accounts");
     }
 
-    @Override
-    public User process(User user) throws Exception {
+    public User process(User user) throws Exception
+    {
+
+        System.out.println("Entering process() of Processor.");
+
         String deptCode = user.getDept();
         String dept = DEPT_NAMES.get(deptCode);
         user.setDept(dept);
